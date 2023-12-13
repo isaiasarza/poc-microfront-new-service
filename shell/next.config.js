5fb48b7b1e08e81ec1d54dc5e256ac0bfd2820c4
IsaiasArza <arza.isaias@gmail.com>
2023-12-13 16:18:58 -0300

    removing env variables

--- shell/next.config.js
+++ shell/next.config.js
@@ -2,11 +2,13 @@ const NextFederationPlugin = require("@module-federation/nextjs-mf");
 // this enables you to use import() and the webpack parser
 // loading remotes on demand, not ideal for SSR
 const remotes = (isServer) => {
-  console.log(process.env);
+  console.log("LOGIN_HOST:", process.env.LOGIN_HOST);
   const location = isServer ? "ssr" : "chunks";
   return {
-    login: `login@${process.env.LOGIN_HOST}/_next/static/${location}/remoteEntry.js`,
-    home: `home@${process.env.HOME_HOST}/_next/static/${location}/remoteEntry.js`,
+    //login: `login@${process.env.LOGIN_HOST}/_next/static/${location}/remoteEntry.js`,
+    login: `login@https://poc-microfront-login-50lpg7u1s-daniarzas-projects.vercel.app/_next/static/${location}/remoteEntry.js`,
+    //home: `home@${process.env.HOME_HOST}/_next/static/${location}/remoteEntry.js`,
+    home: `home@https://poc-microfront-home-1igahlqri-daniarzas-projects.vercel.app/_next/static/${location}/remoteEntry.js`,
     s1: `s1@${process.env.S1_HOST}/_next/static/${location}/remoteEntry.js`,
     s2: `s2@${process.env.S2_HOST}/_next/static/${location}/remoteEntry.js`,
     s3: `s3@${process.env.S3_HOST}/_next/static/${location}/remoteEntry.js`,
